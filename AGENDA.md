ac2ff6f4cc511f79b1f113ff817234906a8d0614
Tom Tarpey <tomtarpeydev@gmail.com>
2020-04-05 15:12:33 +0100

    Day 1 agenda and prep

---
+++ AGENDA.md
@@ -0,0 +1,10 @@
+# Python I Agenda
+
+## Day 1 (Intro to Python Programming)
+- Python resources
+- 5 min break
+- Text and printing
+- Collectrions (Lists)
+- 5 min break
+- Collections (Dictionaries)
+- wrap up
---
+++ day1.py
@@ -0,0 +1,42 @@
+# This is a comment
+
+# lets print a string
+
+# variables
+
+# f strings
+
+# collections
+
+# create an empty list?
+
+# create a list with numbers 1, 2, 3, 4, 5
+
+# add an element 24 to lst1
+
+# print all values in lst2
+
+# while loop
+
+# List Comprehensions
+
+# Create a new list containing the squares of all values in 'numbers'
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+
+# Filtering with a list comprehension
+
+# create a new list of even numbers using the values of the numbers list as inputs
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# create a new list containing only the names that start with 's' make sure they are capitalized (regardless of their original case)
+names = ['Patrick', 'Melquisedeque', 'Bob', 'steve', 'Sam', 'frank', 'shawn']
+
+# Dictionaries
+
+# Create a new dictionary
+
+# access an element via its key
+
+# iterate over dict
+
