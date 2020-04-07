2c79f3ccc582a351f40cfe345b5103156bbb52dc
Tom Tarpey <tomtarpeydev@gmail.com>
2020-04-07 10:03:29 +0100

    pass by done

--- passby.py
+++ passby.py
@@ -12,7 +12,7 @@ print(n)
 ## by ref
 ## list is mutable
 l = [1, 2, 3, 4] # @9923 => @9923
-t = (1, 2, 3, 4) # @9960
+# t = (1, 2, 3, 4) # @9960 => @9960
 
 def mult_lst(l):
     for i in range(len(l)):
@@ -21,6 +21,10 @@ def mult_lst(l):
 print(l)
 print(mult_lst(l))
 print(l)
+
+# print(t)
+# print(mult_lst(t))
+# print(t)
 
 
 
