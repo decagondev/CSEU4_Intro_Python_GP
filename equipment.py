96346470b9485ea772b1e567011bcb745b1278da
Tom <tomtarpeydev@gmail.com>
2020-04-09 10:21:09 +0100

    subclassing

--- equipment.py
+++ equipment.py
@@ -1,3 +1,9 @@
-# make a equipment class with the fields of 
+# make an equipment class with the fields of 
 # name, price, style and weight
 # that inherits from the product class
+from product import Product
+class Equipment(Product):
+    def __init__(self, name, price, style, weight):
+        super().__init__(name, price)
+        self.style = style
+        self.weight = weight
