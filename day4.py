c66c142787cbeb1252de15e4307ba92c5b33f9eb
Tom <tomtarpeydev@gmail.com>
2020-04-09 09:49:31 +0100

    comp and inher

--- day4.py
+++ day4.py
@@ -1,17 +1,40 @@
 # composition and inheritance
 
+
 class Bar:
     def __init__(self, name="bar"):
         self.name = name
 
+a = 12
+b = 23
+c = 19
 
 class Foo:
     def __init__(self):
-        self.bar = [Bar()] * 10
+        self.bars = [Bar()] * 10 # has_a relationship
+        self.numbers = [a, b, c] # has_a
 
     def __str__(self):
         return f"{self.bar}"
 
+
+class Entity():
+    def __init__(self, id, x, y):
+        self.id = id
+        self.x = x
+        self.y = y
+
+    def get_id(self):
+        return self.id
+
+
+my_super = Entity()
+
+
+class Mob(Entity): # is_a relationship
+    def __init__(self, id, x, y):
+        super().__init__(id, x, y)
+        # self.bob = super().get_id()
 
 foo = Foo()
 
